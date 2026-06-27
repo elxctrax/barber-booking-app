be6e0cc340f17468da8dabf0e3ea460098d2f294
Karanjit Kahlon <maxomadedat@gmail.com>
2026-06-26 20:56:51 -0700

    fixed name field sizing

--- components/InputBasic.tsx
+++ components/InputBasic.tsx
@@ -1,5 +1,10 @@
 import { Input } from "@/components/ui/input"
 
 export default function InputBasic() {
-  return <Input placeholder="Enter" className="[width:50px]" />
+  return (
+    <Input 
+      placeholder="Enter your name" 
+      className="[width:300px] [max-width:300px] [margin-top:16px] [padding:12px_16px] rounded-md" 
+    />
+  )
 }
